e187d900ebb4cb5e1897df5dcb3f5d03b4dbc627
Diego Rocha Chagas <drochachagas@gmail.com>
2019-04-22 16:01:13 -0300

    Create post list style

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import { fetchPosts } from '../actions';
+import './PostList.css';
 
 class PostList extends Component {
 	componentDidMount(){
