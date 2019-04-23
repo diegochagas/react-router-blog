d36c9865a54e6575c0c09f60dc265fd34cb444fe
Diego Rocha Chagas <drochachagas@gmail.com>
2019-04-23 11:30:39 -0300

    Create user header component

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import { fetchPosts } from '../actions';
+import { fetchPostsAndUsers } from '../actions';
 import './PostList.css';
+import UserHeader from './UserHeader';
 
 class PostList extends Component {
 	componentDidMount(){
-		this.props.fetchPosts();
+		this.props.fetchPostsAndUsers();
 	}
 
 	renderList(){
@@ -15,6 +16,7 @@ class PostList extends Component {
           <div className="post-description">
             <h2>{post.title}</h2>
             <p>{post.body}</p>
+						<UserHeader userId={post.userId}/>
           </div>
 				</li>
 			);
@@ -34,4 +36,4 @@ const mapStateToProps = state => {
 	return {posts: state.posts};
 }
 
-export default connect(mapStateToProps, {fetchPosts})(PostList);
+export default connect(mapStateToProps, {fetchPostsAndUsers})(PostList);
